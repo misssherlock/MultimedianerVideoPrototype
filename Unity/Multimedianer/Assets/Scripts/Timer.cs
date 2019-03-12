a2e5b7b8d5e9200d843a2a36ddfad7593c47af63
QuackvanQuackson <richard.lindner13@gmail.com>
2019-03-12 13:22:30 +0100

    Clean Code

--- Unity/Multimedianer/Assets/Scripts/Timer.cs
+++ Unity/Multimedianer/Assets/Scripts/Timer.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Timer : MonoBehaviour {
-
+public class Timer : MonoBehaviour 
+{
 	float currentTime = 0f;
     float startingTime = 30f;
     
     public Text TimeDisplay;
 	
-    void Start () {
+    void Start () 
+    {
 		currentTime = startingTime;
 	}
-	
 
 	void Update () {
 		currentTime -= 1 * Time.deltaTime; //jede sekunde
